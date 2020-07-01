29c46c499b333944f0e44fdd32c59ac33848fb8a
Himanshu Sharma <63186590+HimanshuS-coder@users.noreply.github.com>
2020-07-01 12:29:41 +0530

    Slider updated

--- app/src/main/java/com/example/noteme/slider.java
+++ app/src/main/java/com/example/noteme/slider.java
@@ -5,8 +5,11 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class slider extends AppCompatActivity {
     ViewPager viewPager;
@@ -16,37 +19,68 @@ public class slider extends AppCompatActivity {
 
     SliderAdapter sliderAdapter;
 
+    Button nextbutton ,skip ;
+    Button prevbutton;
+    int currentpage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slider);
         viewPager=(ViewPager) findViewById(R.id.viewPager);
         dots=(LinearLayout) findViewById(R.id.dots);
+        nextbutton=findViewById(R.id.nextbutton);
+        prevbutton=findViewById(R.id.prevbutton);
+        skip=findViewById(R.id.skip);
+        skip.setText("Skip");
 
         sliderAdapter = new SliderAdapter(this);
 
         viewPager.setAdapter(sliderAdapter);
 
-        addDotsIndicator();
+        addDotsIndicator(0);
+
+        viewPager.addOnPageChangeListener(viewListener);
+
+        //OnClick Listeners
+
+        nextbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(nextbutton.getText()== "Finish"){
+                    Toast.makeText(slider.this, "FINISH", Toast.LENGTH_SHORT).show();
+                }
+                viewPager.setCurrentItem(currentpage +1);
+            }
+        });
+
+        prevbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(currentpage - 1);
+            }
+        });
 
     }
 
     public void addDotsIndicator (int position) {
 
         mdots = new TextView[3];
+        dots.removeAllViews();
 
         for (int i=0; i<mdots.length; i++){
 
                 mdots[i] =new TextView(this);
-                mdots[i].setText(Html.fromHtml("&#8226",0));
+                mdots[i].setText(Html.fromHtml("&#8226;",0));
                 mdots[i].setTextSize(35);
-                mdots[i].setTextColor(getResources().getColor(R.color.colorGreen));
+                mdots[i].setTextColor(getResources().getColor(R.color.colorGreen,getTheme()));
 
                 dots.addView(mdots[i]);
         }
 
         if (mdots.length > 0){
-            mdots[position].setTextColor(getResources().getColor(R.color.colordarkgreen));
+            mdots[position].setTextColor(getResources().getColor(R.color.colordarkgreen,getTheme()));
         }
     }
 
@@ -60,6 +94,31 @@ public class slider extends AppCompatActivity {
         public void onPageSelected(int position) {
 
             addDotsIndicator(position);
+            currentpage = position;
+
+            if(position==0){
+                nextbutton.setEnabled(true);
+                prevbutton.setEnabled(false);
+                prevbutton.setVisibility(View.INVISIBLE);
+
+                nextbutton.setText("Next");
+                prevbutton.setText("");
+            } else if(position==mdots.length-1){
+                nextbutton.setEnabled(true);
+                prevbutton.setEnabled(true);
+                prevbutton.setVisibility(View.VISIBLE);
+
+                nextbutton.setText("Finish");
+                prevbutton.setText("Back");
+            } else{
+                nextbutton.setEnabled(true);
+                prevbutton.setEnabled(true);
+                prevbutton.setVisibility(View.VISIBLE);
+
+                nextbutton.setText("Next");
+                prevbutton.setText("Back");
+            }
+
         }
 
         @Override
